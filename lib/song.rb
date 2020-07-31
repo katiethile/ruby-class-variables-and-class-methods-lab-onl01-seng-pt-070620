cac5831bb57fc28db8c070d87824804a5004f6a9
Katie Le <lethikatie@gmail.com>
2020-07-31 03:09:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,41 +1,38 @@
 class Song
-
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@artists = []
   @@genres = []
-
-  def initialize(song_name,artist,genre)
-    @name = song_name
+  @@artists = []
+  def initialize(name, artist, genre)
+    @name = name 
     @artist = artist
-    @genre = genre
+    @genre = genre 
     @@count += 1
     @@artists << artist
-    @@genres << genre
-  end
+    @@genres << genre 
+end 
 
-  def self.count 
+  def self.count
     @@count
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
+end 
 
   def self.genres
     @@genres.uniq
-  end
+end 
+
+  def self.artists
+    @@artists.uniq
+end 
 
   def self.genre_count
     genre_count = Hash.new(0)
-    @@genres.each {|genre| genre_count[genre] += 1}
+    @@genres.each {|genres| genre_count[genres] += 1}
     genre_count
-  end
-
+  end 
+  
   def self.artist_count
     artist_count = Hash.new(0)
-    @@artists.each {|artist| artist_count[artist] += 1}
+    @@artists.each {|artists| artist_count[artists] += 1}
     artist_count
-  end
-
-end
+  end 
+end 
